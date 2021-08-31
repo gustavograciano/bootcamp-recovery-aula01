deda49c55af083a8651e87cdc0daee06dff92e73
gustavograciano <gustavograciano18@gmail.com>
2021-08-31 17:49:39 -0300

    Aula1

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeira aula!");
+        }
+
+    }
+}
