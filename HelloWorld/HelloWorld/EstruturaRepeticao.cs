837b41038a6788e8c508ec6bf83b952725b16d87
gustavograciano <gustavograciano18@gmail.com>
2021-09-01 17:00:07 -0300

    Estrutura de decisão aula2

---
+++ HelloWorld/HelloWorld/EstruturaRepeticao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class EstruturaRepeticao
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
--- HelloWorld/HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -6,10 +6,66 @@ namespace HelloWorld
 {
     class HelloWorld
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Primeira aula!");
-        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Meu primeiro codigo C# manual");
+        //    int idade = 32;
+        //    string nome = null;
+        //    bool isAdulto = true;
+        //    decimal valor = 10.55m;
+        //    double valor2 = 10.55;
+        //    char sigla = 'C';
+        //    string estado = "Sao Paulo";
+
+        //    var teste = "Teste";
+
+        //    int idade = 16;
+        //    char sigla = 'A';
+        //    string estado = "Bahia";
+
+        //    // ! negacao && =  AND e || = OR
+        //    if (nome != null)
+        //    {
+        //        nome.ToUpper();
+        //    }
+        //    if (isAdulto != true)
+        //    {
+        //        teste = "Menor de idade";
+        //    }
+        //    else if (estado == "Sao Paulo")
+        //    {
+        //        teste = "Paulista";
+        //    }
+
+        //    Console.WriteLine(teste);
+
+        //    // Switch case
+        //    /*
+        //    switch(idade)
+        //    {
+        //        case < 2: 
+        //            teste = "Nenem";
+        //            break;
+        //        case < 18: 
+        //            teste = "Crianca";
+        //            break;
+        //        case < 65: 
+        //            teste = "Adulto";
+        //            break;
+        //        default: 
+        //            teste = "Melhor idade";
+        //            break;
+        //    }
+
+        //    */
+
+        //    valor2 += 5;
+        //    valor2 -= 5;
+        //    valor2 /= 5;
+        //    valor2 *= 5;
+
+
+        //}
 
     }
 }
